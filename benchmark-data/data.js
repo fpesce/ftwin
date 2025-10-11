9304bfc8c7a7a7bc1c4d3c94d59ebae13cb2353c
github-action-benchmark <github@users.noreply.github.com>
2025-10-11 10:37:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f127f7cc0b1622c61dc9d30c13337f666dfbb4b5

---
+++ benchmark-data/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1760179034019,
+  "repoUrl": "https://github.com/fpesce/ftwin",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "francois.pesce@gmail.com",
+            "name": "Francois Pesce",
+            "username": "fpesce"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f127f7cc0b1622c61dc9d30c13337f666dfbb4b5",
+          "message": "Merge pull request #16 from fpesce/feature/add-linting-workflow\n\nfeat: Add CI workflow for C-project linting and static analysis",
+          "timestamp": "2025-10-11T12:35:27+02:00",
+          "tree_id": "9bbac94a874d1db094c76a3a171eba72d5f19aac",
+          "url": "https://github.com/fpesce/ftwin/commit/f127f7cc0b1622c61dc9d30c13337f666dfbb4b5"
+        },
+        "date": 1760179033489,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "hash_throughput",
+            "value": 1259.38,
+            "unit": "MB/s"
+          },
+          {
+            "name": "checksum_file_throughput",
+            "value": 1146.98,
+            "unit": "MB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
